b2863f9e9705a6f7644cc0c49b14a7c0a085b7a3
Alex <maisilex7@gmail.com>
2021-03-21 17:38:20 +0300

    Init

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  // import logo from './assets/svelte.png'
+  // import Counter from './lib/Counter.svelte'
+  import TailwindCSS from './lib/TailwindCSS.svelte'
+</script>
+
+<TailwindCSS />
+<main  class="min-h-screen flex justify-center items-center">
+  <h1 class="text-8xl">hey svelte!!!</h1>  
+</main>
+
+
