abd35439b611e35a05fe1d69bfb10306f642b300
Alex <maisilex7@gmail.com>
2021-03-22 13:48:59 +0300

    Upd

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,13 @@
   // import logo from './assets/svelte.png'
   // import Counter from './lib/Counter.svelte'
   import TailwindCSS from './lib/TailwindCSS.svelte'
+  // import Navbar from './lib/Navbar.svelte'
 </script>
 
 <TailwindCSS />
-<main  class="min-h-screen flex justify-center items-center">
-  <h1 class="text-8xl">hey svelte!!!</h1>  
+<main  class="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
+  <h1 class="capitalize text-6xl sm:text-8xl">hey svelte!!!</h1>  
+  <p class="my-8">Some Description</p>
 </main>
 
 
